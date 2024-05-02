905339eea4e46f8883604bc27af447bc669c803b
Isaiah19 <nwezeifeanyi93@gmail.com>
2024-05-02 22:50:48 +0000

    0x00-python_variable_annotations

--- 0x00-python_variable_annotations/0-main.py
+++ 0x00-python_variable_annotations/0-main.py
@@ -3,3 +3,7 @@ add = __import__('0-add').add
 
 print(add(1.11, 2.22) == 1.11 + 2.22)
 print(add.__annotations__)
+
+bob@dylan:~$ ./0-main.py
+True
+{'a':  <class 'float'>, 'b': <class 'float'>, 'return': <class 'float'>}
